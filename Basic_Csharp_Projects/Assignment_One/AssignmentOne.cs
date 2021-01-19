0f0c6a47173402e0ad8bf86b4cd84a82c3b36b26
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-18 17:35:36 -0700

    create AssignmentOne and SampleApplication

---
+++ Basic_Csharp_Projects/Assignment_One/AssignmentOne.cs
@@ -0,0 +1,8 @@
+using System;
+
+class WelcomeMessage{
+    static void Main(){
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you!");
+    }
+}
