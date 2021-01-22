ba5d3186d71766823aa9977e6b8acea257fe8739
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-21 19:03:16 -0700

    Create Assignment_Five

---
+++ Basic_Csharp_Projects/Assignment_Five/Assignment_Five/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class LicenseApproval
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your age?");
+        int UserAge = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("have you had any DUIs? true/false");
+        bool UserDui = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("How many speeding tickets due you have?");
+        int UserTickets = Convert.ToInt32(Console.ReadLine());
+
+        bool isValid = (UserAge > 15 && UserDui == false && UserTickets < 3);
+        Console.WriteLine("Qualified?");
+        Console.WriteLine(isValid);
+    }
+}
