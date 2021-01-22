99fe5cbb6b3100c85405ad83481df76713e71185
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-21 20:45:36 -0700

    create Assignment_Eight

---
+++ Basic_Csharp_Projects/Assignment_Eight/Assignment_Eight/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Eight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string StringOne = "John is a man of focus, ";
+            string StringTwo = "commitment, ";
+            string StringThree = "sheer will.";
+            Console.WriteLine(StringOne + StringTwo + StringThree);
+
+            string StringFour = "aaaaaaaa";
+            Console.WriteLine(StringFour.ToUpper());
+
+            StringBuilder sb = new StringBuilder();
+
+            bool EndSb = false;
+            while (!EndSb)
+            {
+                Console.WriteLine("Write the next line. (Type 'DONE' to end)");
+                string line = Console.ReadLine();
+                sb.Append(line + "\n");
+                if (line == "DONE")
+                {
+                    Console.WriteLine("Goodbye!");
+                    EndSb = true;
+                }
+                else
+                {
+                    Console.WriteLine(sb);
+                }
+            }
+        }
+    }
+}
