e5e44a55d93bc3a272d07708eabf4d3e73d6ca47
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-24 15:05:09 -0700

    create Assignment_Twelve

---
+++ Basic_Csharp_Projects/Assignment_Twelve/Assignment_Twelve/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assignment_Twelve
+{
+    class Program
+    {
+        static int methodOne(int args)
+        {
+            return 75 % args;
+        }
+
+        static int methodTwo(int args)
+        {
+            return 66 * args;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Input a number to use for the methods");
+            try
+            {
+                int userNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(methodOne(userNum));
+                Console.WriteLine(methodTwo(userNum));
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
