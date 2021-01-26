d796a36764d88c7d8a7302478bf14286fa017285
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-26 10:36:27 -0700

    create Assignment_Eighteen

---
+++ Basic_Csharp_Projects/Assignment_Eighteen/Assignment_Eighteen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assignment_Eighteen
+{
+    abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public abstract void SayName();
+    }
+
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            sample.SayName();
+        }
+    }
+}
