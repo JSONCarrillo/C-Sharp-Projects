4834a667586da0ea1782f77c4a789b551141e968
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-25 18:00:10 -0700

    create Assignment_Fifteen

---
+++ Basic_Csharp_Projects/Assignment_Fifteen/Assignment_Fifteen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Assignment_Fifteen
+{
+    class Program
+    {
+        static void Method(int x, int y)
+        {
+            int math = x + 12;
+            Console.WriteLine(y);
+        }
+        static void Main(string[] args)
+        {
+            Method(1, 13);
+            Method( x: 1, y: 3);
+        }
+    }
+}
