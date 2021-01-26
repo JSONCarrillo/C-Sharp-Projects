509757dbc8493fecd3c729d412fc55548de0394c
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-25 17:40:04 -0700

    create Assignment_Thirteen

---
+++ Basic_Csharp_Projects/Assignment_Thirteen/Assignment_Thirteen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment_Thirteen
+{
+    class Program
+    {
+        static int Math(int x)
+        {
+            return x * 12;
+        }
+        static int Math( double x)
+        {
+            int answer = Convert.ToInt32(x + 12);
+            return answer;
+        }
+        static int Math(string x)
+        {
+            int y = Convert.ToInt32(x);
+            return y / 3;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math(12));
+            Console.WriteLine(Math(13.5));
+            Console.WriteLine(Math("12"));
+        }
+    }
+}
