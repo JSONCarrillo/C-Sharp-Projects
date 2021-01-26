af1f8b944471fda418d103c128f089473b59e809
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-25 18:32:47 -0700

    Update Program.cs

--- Basic_Csharp_Projects/Assignment_Sixteen/Assignment_Sixteen/Program.cs
+++ Basic_Csharp_Projects/Assignment_Sixteen/Assignment_Sixteen/Program.cs
@@ -2,35 +2,40 @@
 
 namespace Assignment_Sixteen
 {
-    public static class Program
+
+    public static class methodClass
     {
-
-        static void Main(string[] args)
-        {
-            Method(2);
-            Console.WriteLine("Input an integer");
-            int userInt = Convert.ToInt32(Console.ReadLine());
-            Method(userInt);
-            int z = MethodTwo(userInt, out int y);
-            MethodTwo(12);
-            Console.WriteLine($"{y}");
-
-        }
-        static int MethodTwo(int x, out int y)
+        public static int MethodTwo(int x, out int y)
         {
             y = 12 + 32;
             int maths = x + 12;
             return maths;
         }
 
-        static void MethodTwo(int x, int y = 12)
+        public static void MethodTwo(int x, int y = 12)
         {
             Console.WriteLine(x + 24);
 
         }
-        static void Method(int x)
+        public static void Method(int x)
         {
             Console.WriteLine(x / 2);
         }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            methodClass.Method(2);
+            Console.WriteLine("Input an integer");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            methodClass.Method(userInt);
+            int z = methodClass.MethodTwo(userInt, out int y);
+            methodClass.MethodTwo(12);
+            Console.WriteLine($"{y}");
+
+        }
+
     }
 }
