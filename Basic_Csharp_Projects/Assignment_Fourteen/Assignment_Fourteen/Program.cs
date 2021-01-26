87b4de9d5d3604723fcf2a970931e25da31c33c4
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-25 17:49:52 -0700

    Create Assignment_Fourteen

---
+++ Basic_Csharp_Projects/Assignment_Fourteen/Assignment_Fourteen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment_Fourteen
+{
+    class Program
+    {
+        static int Math(int x, int y = 1)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math(2));
+            Console.WriteLine("Choose your first integer");
+            int intOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose your second integer (optional, hit enter to continue)");
+            
+            try
+            {
+                int intTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Math(intOne, intTwo));
+            }
+            catch(Exception)
+            {
+                Console.WriteLine(Math(intOne));
+            }
+        }
+    }
+}
