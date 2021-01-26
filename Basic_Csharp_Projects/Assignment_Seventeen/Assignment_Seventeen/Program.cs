c1f355dbc4c9b2fd6dd3325a412204a7ece661de
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-26 10:22:45 -0700

    create Assignment_Seventeen

---
+++ Basic_Csharp_Projects/Assignment_Seventeen/Assignment_Seventeen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment_Seventeen
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int id;
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newPerson = new Employee();
+
+            newPerson.FirstName = "Sample";
+            newPerson.LastName = "Student";
+
+            newPerson.SayName();
+
+        }
+    }
+}
