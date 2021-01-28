087ab8eb947f8657ec4c2ccdda3e83760396a963
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-28 10:02:37 -0700

    create Assignment_Nineteen

---
+++ Basic_Csharp_Projects/Assignment_Nineteen/Assignment_Nineteen/IQuittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_Nineteen
+{
+    interface IQuittable
+    {
+        public void IQuit()
+        {
+
+        }
+    }
+}
---
+++ Basic_Csharp_Projects/Assignment_Nineteen/Assignment_Nineteen/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Assignment_Nineteen
+{
+    abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public abstract void SayName();
+    }
+
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+
+        public void IQuit()
+        {
+            Console.WriteLine($"{FirstName} {LastName} has quit");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            sample.SayName();
+            sample.IQuit();
+        }
+    }
+}
