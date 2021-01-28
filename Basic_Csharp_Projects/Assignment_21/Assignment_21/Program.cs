890f4747cc7057b7b06857404db4af17b8eb4194
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-28 16:08:07 -0700

    create Assignment_21

--- Basic_Csharp_Projects/Assignment_21/Assignment_21/Program.cs
+++ Basic_Csharp_Projects/Assignment_21/Assignment_21/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Assignment_21
 {
     class Program
     {
+        public class Employee<T>
+        {
+            public List<T> things { get; set; }
+
+
+        }
+
         static void Main(string[] args)
         {
-            
+            Employee<string> names = new Employee<string>();
+            names.things = new List<string>() { "james", "Jimmy", "Jim", "Alf" };
+            Employee<int> id = new Employee<int>();
+            id.things = new List<int>() { 1, 2, 3, 4 };
+
+            foreach (string thing in names.things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in id.things)
+            {
+                Console.WriteLine(thing);
+            }
         }
     }
 }
