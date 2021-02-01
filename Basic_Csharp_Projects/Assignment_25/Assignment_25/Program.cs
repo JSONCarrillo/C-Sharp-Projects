e98deeffc35499184f7cc2c11634dd2eeb9c5de6
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-31 20:08:28 -0700

    Create Assignment_25

---
+++ Basic_Csharp_Projects/Assignment_25/Assignment_25/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\jason\Desktop\\Logs\log.txt", text);
+            using (StreamReader file = File.OpenText(@"C:\Users\jason\Desktop\Folder1\Logs\log.txt"))
+            {
+                string number = "";
+
+                while ((number = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
