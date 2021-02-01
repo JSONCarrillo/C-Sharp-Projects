c0600bd12f31109314a14ad29438202966cfa35f
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-01-31 20:36:31 -0700

    Assignment_27

---
+++ Basic_Csharp_Projects/Assignment_27/Assignment_27/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int currentYear = 2021;
+            Console.WriteLine("The current year is {0}", currentYear);
+
+            var previousYear = new Time("previous Year", 2020);
+            if(previousYear.Year == 2020)
+            {
+                Console.WriteLine($"The previous year is {previousYear.Year}, so glad that's over!");
+            }
+            else
+            {
+                Console.WriteLine($"The previous year is {previousYear.Year}");
+            }
+                
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_Csharp_Projects/Assignment_27/Assignment_27/Time.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_27
+{
+        class Time
+        {
+            public string Name { get; set; }
+            public int Year { get; set; }
+
+            public Time(string name, int year)
+            {
+                Name = name;
+                Year = year;
+            }
+            public Time() : this("Two years ago", 2018)
+            {
+            }
+        }
+}
