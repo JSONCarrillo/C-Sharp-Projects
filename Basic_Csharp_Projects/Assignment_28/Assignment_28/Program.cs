1b216a95ed53308902b5a98e58221de46c515816
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-02-01 18:09:36 -0700

    update Assignment_28

--- Basic_Csharp_Projects/Assignment_28/Assignment_28/Program.cs
+++ Basic_Csharp_Projects/Assignment_28/Assignment_28/Program.cs
@@ -4,9 +4,50 @@ namespace Assignment_28
 {
     class Program
     {
+        public class AgeException : Exception
+        {
+            public AgeException()
+               : base() { }
+            public AgeException(String message)
+                : base(message) { }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your age.");
+            try
+            {
+                bool validAnswer = false;
+                int age = 1;
+                while (!validAnswer)
+                {
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+                }
+                if (age <= 0)
+                {
+                    throw new AgeException();
+                }
+
+                DateTime currentYear = DateTime.Now;
+                int CurrentYear = Convert.ToInt32(currentYear.Year);
+                int YearOfBorn = CurrentYear - age;
+                Console.WriteLine("You were borned in the year of {0}", YearOfBorn);
+                Console.ReadLine();
+            }
+            catch (AgeException)
+            {
+                Console.WriteLine("An error occurred. Please enter positive digitals only, no decimals.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Please contact your System Administrator.");
+                Console.ReadLine();
+                return;
+            }
+
+
         }
     }
 }
