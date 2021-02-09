37901bf492fad7d3c1a1a8c6d05bb3f513916195
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2021-02-08 17:17:07 -0700

    create CodeFirst

---
+++ Basic_Csharp_Projects/CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentsContext())
+            {
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Student: ");
+                var name = Console.ReadLine();
+
+                var student = new Student { Name = name };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                // Display all Blogs from the database
+                var query = from s in db.Students
+                            orderby s.Name
+                            select s;
+
+                Console.WriteLine("All students in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        //public virtual List<Post> Posts { get; set; }
+    }
+
+
+    public class StudentsContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
